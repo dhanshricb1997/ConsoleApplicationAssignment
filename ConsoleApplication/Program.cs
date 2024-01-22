8c0fe5fae79828a38797292e46a579736d408192
Sachin <Sachin@DESKTOP-H5DK26B>
2024-01-22 09:55:09 +0530

    Add project files.

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Write("Enter a number: ");
+if (int.TryParse(Console.ReadLine(), out int number))
+{
+    if (number % 3 == 0 && number % 5 == 0)
+    {
+        Console.WriteLine("Num35");
+    }
+    else if (number % 3 == 0)
+    {
+        Console.WriteLine("Num3");
+    }
+    else if (number % 5 == 0)
+    {
+        Console.WriteLine("Num5");
+    }
+    else
+    {
+        Console.WriteLine("Number does not meet any condition.");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input. Please enter a valid numeric value.");
+}
